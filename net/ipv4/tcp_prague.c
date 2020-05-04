b8f11da5ce6fa449ce489467c8a6634906c2a863
Joakim Skjelbred Misund <joakim.misund@gmail.com>
2020-05-04 16:09:04 +0200

    Set initial alpha to 0 if paced chirping is used.

--- net/ipv4/tcp_prague.c
+++ net/ipv4/tcp_prague.c
@@ -375,8 +375,10 @@ static void prague_init(struct sock *sk)
 		cmpxchg(&sk->sk_pacing_status, SK_PACING_NONE,
 			SK_PACING_NEEDED);
 
-		if (paced_chirping_enabled)
+		if (paced_chirping_enabled) {
+			ca->upscaled_alpha = 0;
 			paced_chirping_init(sk, tp, &ca->pc);
+		}
 
 		prague_reset(tp, ca);
 		return;
